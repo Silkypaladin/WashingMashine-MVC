73e88c9f059b2e5b0ba1669ba4014727bc126608
Silkypaladin <bednarski.szymon99@gmail.com>
2019-12-05 12:39:39 +0100

    Create controller class
    Create MachineController class in controller directory, implement methods given in model CContainer class

---
+++ src/controller/CMachineContainerController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import model.CClothes;
+import model.CContainer;
+import model.CDestroyedClothes;
+import ui.CWashingMaschineView;
+
+public class CMachineContainerController extends CContainer {
+
+    private CClothes clothesToWash;
+    private CWashingMaschineView currentWash;
+
+    public CMachineContainerController(CClothes clothesToWash, CWashingMaschineView currentWash) {
+        this.clothesToWash = clothesToWash;
+        this.currentWash = currentWash;
+        setCurrentLoadLevel(0);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return getCurrentLoadLevel() == 0;
+    }
+
+
+    @Override
+    public boolean load(CClothes clothesToWash) {
+        if(getCurrentLoadLevel()+clothesToWash.getWeight() > getMaxWeightCapacity())
+            return false;
+        else {
+            setCurrentLoadLevel(getCurrentLoadLevel()+clothesToWash.getWeight());
+            return true;
+        }
+    }
+
+    @Override
+    public CClothes unload() {
+        setCurrentLoadLevel(0);
+        return clothesToWash;
+    }
+
+    @Override
+    public void wash(int setWashTemperature) {
+        if (clothesToWash.getWashTemperature() >= setWashTemperature) {
+            clothesToWash.setWashed(true);
+        } else if (clothesToWash.getWashTemperature() < setWashTemperature)
+            clothesToWash = new CDestroyedClothes(getCurrentLoadLevel(),clothesToWash.getWashTemperature());
+    }
+
+    public CClothes getClothesToWash() {
+        return clothesToWash;
+    }
+
+    public void setClothesToWash(CClothes clothesToWash) {
+        this.clothesToWash = clothesToWash;
+    }
+}
